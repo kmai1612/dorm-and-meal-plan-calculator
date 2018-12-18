1b93b4b1139a83fa07b6a44b91bcdfb5365035ff
kmai1612 <45944460+kmai1612@users.noreply.github.com>
2018-12-17 23:06:49 -0600

    Create mdlsample.vb

---
+++ mdlsample.vb
@@ -0,0 +1,13 @@
+Module mdlSample
+
+    'Public constants for Dormitory prices and weedkly meal plan prices
+    Public Const Allen_Hall As Decimal = 2500D
+    Public Const Pike_Hall As Decimal = 2200D
+    Public Const Farthing_Hall As Decimal = 2100D
+    Public Const UniversitySuites As Decimal = 2800D
+
+    Public Const Meal_Plan_7 As Decimal = 1560D
+    Public Const Meal_Plan_14 As Decimal = 2095D
+    Public Const Meal_Plan_Unilimited As Decimal = 2500D
+
+End Module
